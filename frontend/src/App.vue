83698384fa0d27f42301fb0ccacc4eac8abd51fa
Pargorn Ruasijan <pargornruasijan@gmail.com>
2022-09-23 23:52:03 +0700

    add: router register

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <nav class="text-center">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="register">
+    <h1>Register page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
